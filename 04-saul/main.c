cca9da47b33a95f93a9528a8752b5ec356f0fd09
Isabell Egloff <iseg159@t-online.de>
2025-11-18 03:18:53 +0100

    finish exercise 04

--- 04-saul/main.c
+++ 04-saul/main.c
@@ -13,10 +13,13 @@
 #include "saul_reg.h"
 #include "board.h"
 
-#define TEMPERATURE_THRESHOLD 2400 /* factor of 10^-2 */
+#define TEMPERATURE_THRESHOLD 2500 /* factor of 10^-2 */
+#define Z_FLIPPED -1000
+#define ACCEL_JITTER 100
 
 int main(void)
 {
+    ztimer_sleep(ZTIMER_MSEC, 1000);    // wait pyterm for connection
     puts("SAUL example application");
 
     /* start by finding a temperature sensor in the system */
@@ -30,6 +33,14 @@ int main(void)
     }
 
     /* [TASK 3: find your device here] */
+    saul_reg_t *accel_sensor = saul_reg_find_type(SAUL_SENSE_ACCEL);
+    if (!accel_sensor) {
+        puts("No accelerometer present");
+        return 1;
+    }
+    else {
+        printf("Found accelerometer device: %s\n", accel_sensor->name);
+    }
 
     /* record the starting time */
     ztimer_now_t last_wakeup = ztimer_now(ZTIMER_MSEC);
@@ -39,7 +50,14 @@ int main(void)
         phydat_t temperature;
         int dimensions = saul_reg_read(temp_sensor, &temperature);
         if (dimensions < 1) {
-            puts("Error reading a value from the device");
+            puts("Error reading a value from the temperature sensor");
+            break;
+        }
+
+        phydat_t acceleration;
+        int acc_dim = saul_reg_read(accel_sensor, &acceleration);
+        if (acc_dim < 1) {
+            puts("Error reading a value from the accelerometer");
             break;
         }
 
@@ -47,6 +65,7 @@ int main(void)
         phydat_dump(&temperature, dimensions);
 
         /* [TASK 3: perform the acceleration read here ] */
+        phydat_dump(&acceleration, acc_dim);
 
         /* check if the temperature value is above the threshold */
         if (temperature.val[0] >= TEMPERATURE_THRESHOLD) {
@@ -54,6 +73,15 @@ int main(void)
         }
         else {
             LED0_OFF;
+        }
+
+        // check if the board has been flipped 180 °
+        if (acceleration.val[2] > Z_FLIPPED - ACCEL_JITTER
+            && acceleration.val[2] < Z_FLIPPED + ACCEL_JITTER) {
+            LED1_ON;
+        }
+        else {
+            LED1_OFF;
         }
 
         /* wait for 500 ms */
