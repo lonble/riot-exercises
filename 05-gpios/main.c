df4573f32dcb341e1359993f4b4d1ed932315d61
Lonble <74650029+lonble@users.noreply.github.com>
2025-11-25 17:52:02 +0100

    finish exercise 05

--- 05-gpios/main.c
+++ 05-gpios/main.c
@@ -13,17 +13,39 @@
 #include "periph/gpio.h"
 
 /* [TASK 1: define led0 here] */
+gpio_t led0 = GPIO_PIN(1, 9);
 
 /* [TASK 2: define button and led1 here] */
+gpio_t led1 = GPIO_PIN(1, 10);
+gpio_t button = GPIO_PIN(1, 2);
 
 /* [TASK 2: write the callback function here] */
-
+void button_callback (void *arg)
+{
+    (void) arg; /* the argument is not used */
+    if (!gpio_read(button)) {
+        gpio_set(led1);
+    } else {
+        gpio_clear(led1);
+    }
+}
 
 int main(void)
 {
     puts("GPIOs example.");
 
     /* [TASK 1: initialize and use led0 here] */
+    gpio_init(led0, GPIO_OUT);
+    gpio_init(led1, GPIO_OUT);
+    gpio_clear(led0);
+    gpio_clear(led1);
+
+    gpio_init_int(button, GPIO_IN_PU, GPIO_BOTH, button_callback, NULL);
+    while (1)
+    {
+        gpio_toggle(led0);
+        ztimer_sleep(ZTIMER_MSEC, 500);
+    }
 
     return 0;
 }
