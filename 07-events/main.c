71f1192be5318f92cff07d521275fd6f696c8fff
Lonble <74650029+lonble@users.noreply.github.com>
2025-11-27 00:00:13 +0100

    finish exercise 07

--- 07-events/main.c
+++ 07-events/main.c
@@ -14,22 +14,47 @@
 #include "event.h"
 
 /* [TASK 2: include Event Thread header here] */
+#include "event/thread.h"
 
 #include "ztimer.h"
 
 /* [TASK 2: create event handler here] */
+void event_handler(event_t *event) {
+    (void)event; /* Not used */
 
-/* [TASK 2: instantiate queue and event here] */
+    ztimer_now_t start = ztimer_now(ZTIMER_MSEC);
 
-void button_callback(void *arg)
-{
-    (void) arg;    /* Not used */
+    /* wait until 2000 ms have passed */
+    while (ztimer_now(ZTIMER_MSEC) - start < 2000) { }
 
-    /* [TASK 1 and 2: implement interrupt routing here] */
+    LED0_TOGGLE;
+    puts("Done");
 }
 
-int main(void)
-{
+/* [TASK 2: instantiate queue and event here] */
+event_t event = { .handler = event_handler };
+
+// void button_callback(void *arg) {
+//     (void)arg; /* Not used */
+
+//     /* [TASK 1 and 2: implement interrupt routing here] */
+//     /* get the current time */
+//     ztimer_now_t start = ztimer_now(ZTIMER_MSEC);
+
+//     /* wait until 2000 ms have passed */
+//     while (ztimer_now(ZTIMER_MSEC) - start < 2000) { }
+
+//     LED0_TOGGLE;
+//     puts("Done");
+// }
+
+void button_callback(void *arg) {
+    (void)arg; /* Not used */
+
+    event_post(EVENT_PRIO_LOWEST, &event);
+}
+
+int main(void) {
     puts("Threads and event queue example.");
 
     /* Setup button callback */
