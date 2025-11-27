8fa6f928320aa36ee5d4a9f6a9d513b98556d7a0
Lonble <74650029+lonble@users.noreply.github.com>
2025-11-27 20:59:40 +0100

    modify exercise 03 and it's note

--- 03-shell/main.c
+++ 03-shell/main.c
@@ -7,8 +7,8 @@
  */
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include "shell.h"
 #include "board.h"
@@ -38,6 +38,29 @@ int toggle_command(int argc, char **argv) {
     return 0;
 }
 
+int hex_command(int argc, char **argv) {
+    if (argc != 2) {
+        printf("usage: %s <string>\n", argv[0]);
+        return 1;
+    }
+    int len = 0;
+    for (char const *p = argv[1]; *p != '\0'; ++p) {
+        int first = *p >> 4;
+        int second = *p & ((1 << 4) - 1);
+        printf("%x%x", first, second);
+        ++len;
+        if ((len & 15) != 0) {
+            putchar(' ');
+        } else {
+            putchar('\n');
+        }
+    }
+    if ((len & 15) != 0) {
+        putchar('\n');
+    }
+    return 0;
+}
+
 int echo_command(int argc, char **argv) {
     /* check that the command is called correctly */
     if (argc != 2) {
@@ -60,8 +83,40 @@ int main(void) {
     /* buffer to read commands */
     char line_buf[SHELL_DEFAULT_BUFSIZE];
 
+    shell_command_t commands[] = {
+        {
+            .name = "hex",
+            .desc = "Print the hexadecimal form of the string.",
+            .handler = hex_command,
+        },
+        { NULL, NULL, NULL },
+    };
+
+    // run the first 3 commands manually
+    for (int iter = 0; iter < 3; ++iter) {
+        putchar('>');
+        putchar(' ');
+        fflush(stdout);
+        int len = shell_readline(line_buf, SHELL_DEFAULT_BUFSIZE);
+        if (len == EOF) {
+            puts("shell: EOF reached. exiting");
+            break;
+        } else if (len == -ENOBUFS) {
+            puts("shell: maximum line length exceeded");
+            continue;
+        } else {
+            int rev = shell_handle_input_line(commands, line_buf);
+            if (rev != 0) {
+                printf("shell: return value of last command is %d\n", rev);
+            }
+            continue;
+        }
+    }
+
+    puts("switch to built-in shell");
+
     /* run the shell, this will block the thread waiting for incoming commands */
-    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+    shell_run_forever(commands, line_buf, SHELL_DEFAULT_BUFSIZE);
 
     return 0;
 }
--- 03-shell/note.md
+++ 03-shell/note.md
@@ -1,5 +1,28 @@
-1. `SHELL_COMMAND`是个宏，创建一个结构体，然后使用gcc attribute把它放到指定内存区域。**它必须写在全局位置**
-2. `shell_run()`会阻塞当前线程
-3. `SHELL_DEFAULT_BUFSIZE`是128
-4. shell会回显命令
-5. 命令提示符有BUG，当删除命令的第一个字符后，提示符也会被删除
+- 有两种方法创建命令
+  1. `shell_command_t`数组
+     - `shell_command_t`是结构体，保存命令名称、描述、回调函数
+     - **用空结构体来标识数组末尾**
+     - 把数组传入命令处理函数
+  2. `SHELL_COMMAND`
+     - 这是个宏，创建一个结构体，然后使用gcc attribute把它放到指定内存区域
+     - **它必须写在全局位置**
+     - 命令处理函数的`commands`参数可以为`NULL`
+     - 这是实验性功能，不建议使用
+- `SHELL_DEFAULT_BUFSIZE`是`128`
+- `STDIO_RX_BUFSIZE`是`64`
+- `STDIO_RX_BUFSIZE`必须是2的整数幂，否则有BUG导致标准输入几乎不可用
+- shell默认回显命令，可以通过定义宏`CONFIG_SHELL_NO_ECHO`为`1`关闭回显
+- `shell_readline()`有BUG，当终端指针位于命令第一个字符时，会重置到行首
+- 使用模块`shell_cmds_default`自动添加其他已使用模块的内置命令
+- shell内置`help`命令，打印其他所有命令的名称和描述
+- shell命令可以手动解析运行，灵活度高，可以处理命令返回值
+  1. 使用`shell_readline()`获取输入，通过返回值处理EOF、缓冲区溢出问题。等待输入期间会阻塞线程
+  2. 使用`shell_handle_input_line()`运行命令，函数返回值即是命令返回值
+  3. 重复这些步骤以持续处理命令
+- shell命令也可以自动运行，但无法处理返回值
+  - `shell_run_once()`循环读取命令并执行，如果读到EOF则退出
+  - `shell_run_forever()`循环调用`shell_run_once()`，永远不会退出
+  - `shell_run()`是`shell_run_forever()`的别名
+  - 因为`pyterm`不会把EOF传递给板子，所以这两个函数表现基本相同，都不会退出
+- 
+    
